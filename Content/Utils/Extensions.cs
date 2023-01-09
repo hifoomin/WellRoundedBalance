921564bad963b761f820051a03465682f747e01c
pseudopulse <psuedopulse@protonmail.com>
2023-01-09 16:25:38 -0600

    true

--- Content/Utils/Extensions.cs
+++ Content/Utils/Extensions.cs
@@ -5,5 +5,29 @@ namespace WellRoundedBalance.Utils {
         public static PickupIndex GetPickupIndex(this ItemDef def) {
             return PickupCatalog.FindPickupIndex(def.itemIndex);
         }
+
+        public static void RemoveComponent<T>(this GameObject go) where T : Component {
+            GameObject.Destroy(go.GetComponent<T>());
+        }
+
+        public static void RemoveComponents<T>(this GameObject go) where T : Component {
+            T[] coms = go.GetComponents<T>();
+            for (int i = 0; i < coms.Length; i++) {
+                GameObject.Destroy(coms[i]);
+            }
+        }
+
+        public static T GetRandom<T>(this List<T> list, Xoroshiro128Plus rng = null) {
+            if (rng == null) {
+                return list[UnityEngine.Random.RandomRangeInt(0, list.Count)];
+            }
+            else {
+                return list[rng.RangeInt(0, list.Count)];
+            }
+        }
+
+        public static string ToPercentage(this float self) {
+            return (self * 100).ToString() + "%";
+        }
     }
 }
