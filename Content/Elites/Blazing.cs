782e13d3a33a5d24f4c0b3463b45e0d72e954fa7
hifoomin <gangster07@protonmail.com>
2023-01-11 22:40:16 +0100

    help

---
+++ Content/Elites/Blazing.cs
@@ -0,0 +1,103 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System;
+using UnityEngine;
+using WellRoundedBalance.Enemies;
+
+namespace WellRoundedBalance.Elites
+{
+    internal class Blazing : EnemyBase
+    {
+        public static BuffDef useless;
+        public override string Name => "::: Elites :: Blazing";
+
+        public override void Init()
+        {
+            useless = ScriptableObject.CreateInstance<BuffDef>();
+            useless.name = "Useless Buff";
+            useless.isHidden = true;
+            base.Init();
+        }
+
+        public override void Hooks()
+        {
+            CharacterBody.onBodyStartGlobal += CharacterBody_onBodyStartGlobal;
+            IL.RoR2.GlobalEventManager.OnHitEnemy += GlobalEventManager_OnHitEnemy;
+            On.RoR2.CharacterBody.UpdateFireTrail += CharacterBody_UpdateFireTrail;
+            IL.RoR2.CharacterBody.UpdateFireTrail += CharacterBody_UpdateFireTrail1;
+            Changes();
+        }
+
+        private void CharacterBody_UpdateFireTrail(On.RoR2.CharacterBody.orig_UpdateFireTrail orig, CharacterBody self)
+        {
+            self.fireTrail.radius = 5f * self.radius;
+            orig(self);
+        }
+
+        private void CharacterBody_onBodyStartGlobal(CharacterBody body)
+        {
+            if (body.HasBuff(RoR2Content.Buffs.AffixRed))
+            {
+                var sfp = body.GetComponent<SpawnFirePools>();
+                if (sfp == null)
+                {
+                    body.gameObject.AddComponent<SpawnFirePools>();
+                }
+            }
+        }
+
+        private void CharacterBody_UpdateFireTrail1(ILContext il)
+        {
+            ILCursor c = new(il);
+
+            if (c.TryGotoNext(MoveType.Before,
+                x => x.MatchLdcR4(1.5f)))
+            {
+                c.Next.Operand = 1.75f;
+            }
+            else
+            {
+                Main.WRBLogger.LogError("Failed to apply Blazing Elite Firetrail Damage hook");
+            }
+        }
+
+        private void GlobalEventManager_OnHitEnemy(ILContext il)
+        {
+            ILCursor c = new(il);
+
+            if (c.TryGotoNext(MoveType.Before,
+                x => x.MatchLdsfld(typeof(RoR2Content.Buffs), "AffixRed")))
+            {
+                c.Remove();
+                c.Emit<Blazing>(OpCodes.Ldsfld, nameof(useless));
+            }
+            else
+            {
+                Main.WRBLogger.LogError("Failed to apply Blazing Elite On Hit hook");
+            }
+        }
+
+        private void Changes()
+        {
+            var trail = Utils.Paths.GameObject.FireTrail.Load<GameObject>().GetComponent<DamageTrail>();
+            var trailVFX = Utils.Paths.GameObject.FireTrailSegment.Load<GameObject>();
+            var trailPS = trailVFX.GetComponent<ParticleSystem>();
+            var trailDoT = trailVFX.GetComponent<DestroyOnTimer>();
+
+            trail.pointLifetime = 5f;
+            // trail.radius = 5f;
+
+            trailDoT.duration = 5.1f;
+
+            var main = trailPS.main;
+            main.duration = 5f;
+            var startSize = main.startSize;
+            startSize.mode = ParticleSystemCurveMode.Constant;
+            startSize.constant = 5f;
+        }
+    }
+
+    public class SpawnFirePools : MonoBehaviour
+    {
+    }
+}
---
+++ Content/Elites/ChangeStats.cs
@@ -0,0 +1,76 @@
+﻿using static RoR2.CombatDirector;
+using System.Linq;
+using System;
+using WellRoundedBalance.Enemies;
+
+namespace WellRoundedBalance.Elites
+{
+    public class ChangeStats : EnemyBase
+    {
+        public static float Tier1Cost;
+        public static float Tier1HonorCost;
+        public static float Tier2Cost;
+        public override string Name => "::: Elites : Stat Changes";
+
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        public override void Hooks()
+        {
+            On.RoR2.CombatDirector.Init += Changes;
+        }
+
+        public static void Changes(On.RoR2.CombatDirector.orig_Init orig)
+        {
+            orig();
+            foreach (EliteTierDef eliteTierDef in eliteTiers)
+            {
+                if (eliteTierDef != null && eliteTierDef.eliteTypes.Length > 0)
+                {
+                    List<EliteDef> eliteDefList = eliteTierDef.eliteTypes.ToList();
+                    /*
+                    if (eliteDefList.Contains(RoR2Content.Elites.Fire))
+                    {
+                        eliteTierDef.costMultiplier = Tier1Cost;
+                    }
+
+                    if (eliteDefList.Contains(RoR2Content.Elites.FireHonor))
+                    {
+                        eliteTierDef.costMultiplier = Tier1HonorCost;
+                    }
+
+                    if (eliteDefList.Contains(RoR2Content.Elites.Poison))
+                    {
+                        eliteTierDef.costMultiplier = Tier2Cost;
+                    }
+                    */
+                    // costs might be changed later
+                    foreach (EliteDef eliteDef in eliteDefList)
+                    {
+                        if (eliteDef != null)
+                        {
+                            if (eliteDef.name.IndexOf("honor", StringComparison.OrdinalIgnoreCase) >= 0)
+                            {
+                                var eliteString = Language.GetString(eliteDef.modifierToken);
+                                // Main.WRBLogger.LogError("elitedef is + " + eliteString + " (honor)");
+                                // eliteDef.healthBoostCoefficient = health;
+                                // left default for now
+                                eliteDef.damageBoostCoefficient = 1f;
+                            }
+                            else
+                            {
+                                var eliteString = Language.GetString(eliteDef.modifierToken);
+                                // Main.WRBLogger.LogError("elitedef is + " + eliteString);
+                                // eliteDef.healthBoostCoefficient = health;
+                                // left default for now
+                                eliteDef.damageBoostCoefficient = 1f;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
