54a7cd1c58da75f2862262b54a199afa8e2d0ae3
hifoomin <gangster07@protonmail.com>
2023-03-05 20:58:01 +0100

    why does it do no damage ft. void seers dont scale hyperbolically and kill you

--- Content/Items/Greens/Shuriken.cs
+++ Content/Items/Greens/Shuriken.cs
@@ -1,5 +1,6 @@
 ﻿using MonoMod.Cil;
 using System;
+using UnityEngine.Events;
 
 namespace WellRoundedBalance.Items.Greens
 {
@@ -10,18 +11,18 @@ namespace WellRoundedBalance.Items.Greens
 
         public override string PickupText => "Activating your Primary skill also throws a shuriken. Recharges over time.";
 
-        public override string DescText => "Activating your <style=cIsUtility>Primary skill</style> also throws a <style=cIsDamage>shuriken</style> that deals <style=cIsDamage>400%</style> <style=cStack>(+200% per stack)</style> base damage. The <style=cIsDamage>shuriken</style> reloads over <style=cIsUtility>10</style> seconds.";
+        public override string DescText => "Activating your <style=cIsUtility>Primary skill</style> also throws a <style=cIsDamage>shuriken</style> that deals <style=cIsDamage>" + d(baseDamage) + "</style> <style=cStack>(+" + d(damagePerStack) + " per stack)</style> base damage. The <style=cIsDamage>shuriken</style> reloads over <style=cIsUtility>10</style> seconds.";
 
-        [ConfigField("Base Damage", "Decimal.", 4f)]
+        [ConfigField("Base Damage", "Decimal.", 5f)]
         public static float baseDamage;
 
-        [ConfigField("Damage Per Stack", "Decimal.", 2f)]
+        [ConfigField("Damage Per Stack", "Decimal.", 2.5f)]
         public static float damagePerStack;
 
         [ConfigField("Projectile Lifetime", "", 10f)]
         public static float projectileLifetime;
 
-        [ConfigField("Projectile Speed", "", 100f)]
+        [ConfigField("Projectile Speed", "", 80f)]
         public static float projectileSpeed;
 
         [ConfigField("Enable Boomerang?", "", true)]
@@ -33,10 +34,10 @@ namespace WellRoundedBalance.Items.Greens
         [ConfigField("Enable Homing?", "", false)]
         public static bool enableHoming;
 
-        [ConfigField("Size Multiplier", "", 1f)]
+        [ConfigField("Size Multiplier", "Not actually to scale with Vanilla, I'm not sure why.", 2f)]
         public static float sizeMultiplier;
 
-        [ConfigField("Distance Multiplier", "", 0.4f)]
+        [ConfigField("Distance Multiplier", "", 0.5f)]
         public static float distanceMultiplier;
 
         public float timer;
@@ -48,11 +49,11 @@ namespace WellRoundedBalance.Items.Greens
 
         public override void Hooks()
         {
-            Changes();
             On.RoR2.PrimarySkillShurikenBehavior.FixedUpdate += PrimarySkillShurikenBehavior_FixedUpdate;
             IL.RoR2.PrimarySkillShurikenBehavior.FixedUpdate += PrimarySkillShurikenBehavior_FixedUpdate1;
             IL.RoR2.PrimarySkillShurikenBehavior.FireShuriken += PrimarySkillShurikenBehavior_FireShuriken;
             On.RoR2.PrimarySkillShurikenBehavior.GetRandomRollPitch += PrimarySkillShurikenBehavior_GetRandomRollPitch;
+            Changes();
         }
 
         private Quaternion PrimarySkillShurikenBehavior_GetRandomRollPitch(On.RoR2.PrimarySkillShurikenBehavior.orig_GetRandomRollPitch orig, PrimarySkillShurikenBehavior self)
@@ -91,8 +92,8 @@ namespace WellRoundedBalance.Items.Greens
                 {
                     return 1;
                 });
-                c.Index += 1;
                 // stack => 1
+                c.Index += 1;
                 c.EmitDelegate<Func<int, int>>((useless) =>
                 {
                     return 0;
@@ -120,16 +121,25 @@ namespace WellRoundedBalance.Items.Greens
             }
         }
 
+        public static BoomerangProjectile boomerangProjectile;
+        public static ProjectileOverlapAttack projectileOverlapAttack;
+        public static GameObject shuriken;
+        public UnityEvent UnityGames = new();
+
         private void Changes()
         {
             var shurikenProjectile = Utils.Paths.GameObject.ShurikenProjectile.Load<GameObject>();
+            shuriken = shurikenProjectile;
             var projectileSimple = shurikenProjectile.GetComponent<ProjectileSimple>();
             projectileSimple.lifetime = projectileLifetime;
             projectileSimple.desiredForwardSpeed = projectileSpeed;
 
-            shurikenProjectile.transform.localScale *= sizeMultiplier * 0.2f;
+            shurikenProjectile.transform.localScale = new Vector3(sizeMultiplier / 7f, sizeMultiplier / 7f, sizeMultiplier / 7f);
 
             var projectileSingleTargetImpact = shurikenProjectile.GetComponent<ProjectileSingleTargetImpact>();
+
+            var sphereCollider = shuriken.GetComponent<SphereCollider>();
+            sphereCollider.radius = sizeMultiplier / 5f;
 
             if (!enableHoming)
             {
@@ -143,27 +153,57 @@ namespace WellRoundedBalance.Items.Greens
 
             if (enableBoomerang)
             {
-                var boomerangProjectile = shurikenProjectile.AddComponent<BoomerangProjectile>();
+                boomerangProjectile = shurikenProjectile.AddComponent<BoomerangProjectile>();
                 boomerangProjectile.travelSpeed = projectileSpeed;
                 boomerangProjectile.charge = 1f;
-                boomerangProjectile.transitionDuration = projectileLifetime / 2f;
+                boomerangProjectile.transitionDuration = projectileLifetime / 3f;
                 boomerangProjectile.canHitCharacters = false;
-                boomerangProjectile.canHitWorld = false;
-                boomerangProjectile.distanceMultiplier = 1f;
+                boomerangProjectile.canHitWorld = true;
+                boomerangProjectile.distanceMultiplier = distanceMultiplier;
+                boomerangProjectile.impactSpark = Utils.Paths.GameObject.ShurikenImpact.Load<GameObject>();
 
-                projectileSingleTargetImpact.enabled = false;
+                UnityGames.AddListener(OnFlyBack);
             }
 
             if (enablePierce)
             {
-                var projectileOverlapAttack = shurikenProjectile.AddComponent<ProjectileOverlapAttack>();
+                projectileOverlapAttack = shurikenProjectile.AddComponent<ProjectileOverlapAttack>();
                 projectileOverlapAttack.damageCoefficient = 1f;
-                projectileOverlapAttack.impactEffect = null;
+                projectileOverlapAttack.impactEffect = Utils.Paths.GameObject.ShurikenImpact.Load<GameObject>();
                 projectileOverlapAttack.forceVector = new Vector3(0f, 0f, 0f);
                 projectileOverlapAttack.fireFrequency = 60f;
-                projectileOverlapAttack.resetInterval = 60f;
+                projectileOverlapAttack.resetInterval = -1f;
 
-                projectileSingleTargetImpact.enabled = false;
+                UnityEngine.Object.Destroy(projectileSingleTargetImpact);
+
+                var projectileDotZone = shurikenProjectile.AddComponent<ProjectileDotZone>();
+                projectileDotZone.damageCoefficient = 1f;
+                projectileDotZone.attackerFiltering = AttackerFiltering.NeverHitSelf;
+                projectileDotZone.impactEffect = Utils.Paths.GameObject.OmniImpactVFXSlash.Load<GameObject>();
+                projectileDotZone.forceVector = new Vector3(0f, 0f, 0f);
+                projectileDotZone.overlapProcCoefficient = 1f;
+                projectileDotZone.fireFrequency = 30f;
+                projectileDotZone.resetFrequency = 10f;
+                projectileDotZone.lifetime = -1f;
+            }
+
+            if (enablePierce || enableBoomerang)
+            {
+                GameObject hitBox = new("hitBox");
+                hitBox.AddComponent<HitBox>();
+                hitBox.transform.parent = shuriken.transform;
+                var hitBoxGroup = shuriken.AddComponent<HitBoxGroup>();
+                hitBoxGroup.hitBoxes = new HitBox[] { hitBox.GetComponent<HitBox>() };
+
+                hitBox.transform.localScale = new Vector3(sizeMultiplier, sizeMultiplier, sizeMultiplier);
+            }
+        }
+
+        public void OnFlyBack()
+        {
+            if (boomerangProjectile && projectileOverlapAttack && UnityGames != null)
+            {
+                projectileOverlapAttack.ResetOverlapAttack();
             }
         }
     }
