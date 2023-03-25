4978b25117a9ff13a30e22fd053e51334a289725
hifoomin <gangster07@protonmail.com>
2023-03-25 18:41:56 +0100

    bad code warning?

--- Content/Artifacts/New/Arrangement.cs
+++ Content/Artifacts/New/Arrangement.cs
@@ -6,7 +6,7 @@
 
         public override string ArtifactLangTokenName => "ARRANGEMENT";
 
-        public override string ArtifactDescription => "Category chests are much more common. Every run, a category chest will be more common than others.";
+        public override string ArtifactDescription => "Category chests are much more common. Category chests have unique spawn rates from each other.";
 
         public override Sprite ArtifactEnabledIcon => Main.wellroundedbalance.LoadAsset<Sprite>("texBuffHappiestMaskReady.png");
 
@@ -36,12 +36,14 @@
             orig(self);
             var categories = self.interactableCategories.categories;
             var chest = Utils.Paths.InteractableSpawnCard.iscChest1.Load<InteractableSpawnCard>();
-            var foundCategoryChest = false;
             float chestWeight = 1f;
-            for (int i = 0; i < categories.Length && !foundCategoryChest; i++)
+            int categoryChestsFound = 0;
+            int totalCategoryChests = GetTotalCategoryChestCount(categories);
+
+            for (int i = 0; i < categories.Length && categoryChestsFound < totalCategoryChests; i++)
             {
                 var categoryIndex = categories[i];
-                for (int j = 0; j < categoryIndex.cards.Length; j++)
+                for (int j = 0; j < categoryIndex.cards.Length && categoryChestsFound < totalCategoryChests; j++)
                 {
                     var cardIndex = categoryIndex.cards[j];
                     if (cardIndex.spawnCard == chest)
@@ -52,11 +54,28 @@
                     {
                         Logger.LogError("Found CategoryChest " + cardIndex.spawnCard.name);
                         cardIndex.selectionWeight = Mathf.RoundToInt(cardIndex.selectionWeight * chestWeight * Run.instance.treasureRng.RangeFloat(3.5f, 8f));
-                        foundCategoryChest = true;
-                        break;
+                        categoryChestsFound++;
                     }
                 }
             }
+        }
+
+        private int GetTotalCategoryChestCount(DirectorCardCategorySelection.Category[] categories)
+        {
+            int count = 0;
+            for (int i = 0; i < categories.Length; i++)
+            {
+                var categoryIndex = categories[i];
+                for (int j = 0; j < categoryIndex.cards.Length; j++)
+                {
+                    var cardIndex = categoryIndex.cards[j];
+                    if (cardIndex.spawnCard.name.Contains("CategoryChest"))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
         }
 
         public override void Init()
