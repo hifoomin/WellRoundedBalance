12bc3b7c0ffd56caaf2e2b9f99807b85c66ac14c
hifoomin <gangster07@protonmail.com>
2023-03-30 17:00:18 +0200

    :smirk_cat:

--- Content/Mechanics/Director/CombatDirector.cs
+++ Content/Mechanics/Director/CombatDirector.cs
@@ -10,7 +10,7 @@
         [ConfigField("Credit Multiplier", "", 1.25f)]
         public static float creditMultiplier;
 
-        [ConfigField("Elite Bias Multiplierr", "", 0.2f)]
+        [ConfigField("Elite Bias Multiplier", "", 0.9f)]
         public static float eliteBiasMultiplier;
 
         [ConfigField("Credit Multiplier for each Mountain Shrine", "", 1.05f)]
@@ -34,6 +34,7 @@
         private bool CombatDirector_Spawn(On.RoR2.CombatDirector.orig_Spawn orig, RoR2.CombatDirector self, SpawnCard spawnCard, EliteDef eliteDef, Transform spawnTarget, DirectorCore.MonsterSpawnDistance spawnDistance, bool preventOverhead, float valueMultiplier, DirectorPlacementRule.PlacementMode placementMode)
         {
             self.monsterCredit += (0.3f * Mathf.Sqrt(Run.instance.difficultyCoefficient)) + ((spawnCard.directorCreditCost - 50) / 50);
+
             return orig(self, spawnCard, eliteDef, spawnTarget, spawnDistance, preventOverhead, valueMultiplier, placementMode);
         }
 
